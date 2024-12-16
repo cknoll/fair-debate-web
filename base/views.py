87fd5d00788b5ec98cb7bf4ff5a29e9397ef060a
Carsten Knoll <carsten.knoll@posteo.de>
2024-12-17 00:25:59 +0100

    feat: allow access to reason of FileNotFoundError

--- base/views.py
+++ base/views.py
@@ -265,8 +265,11 @@ class ShowDebateView(View):
         try:
             ddl = fdmd.load_repo(settings.REPO_HOST_DIR, debate_key, ctb_list=ctb_list, new_debate=new_debate)
         except FileNotFoundError:
-            msg = f"No debate with key `{debate_key}` could be found."
-            return error_page(request, title="Not Found", msg=msg, status=404)
+            if settings.CATCH_EXCEPTIONS:
+                msg = f"No debate with key `{debate_key}` could be found."
+                return error_page(request, title="Not Found", msg=msg, status=404)
+            else:
+                raise
         return self.render_result_from_html(request, ddl)
 
     @method_decorator(login_required(login_url=f"/{settings.LOGIN_URL}"))
