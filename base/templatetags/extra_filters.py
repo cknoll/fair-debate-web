c3d8bdda4315436d93c8d32873b6499d5ceb3f7a
Carsten Knoll <carsten.knoll@posteo.de>
2026-05-03 02:15:08 +0200

    refactor: replace HTML comment whitespace hacks with custom template tag
    # Last 3 commits:
    
    # 1322e00 feat: display user role in debate view if authenticated
    # c01fdf6 refactor (main_landing_page.html): improve template formatting
    # 01a79b1 docs: simplify password placeholders and reorganize documentation

--- base/templatetags/extra_filters.py
+++ base/templatetags/extra_filters.py
@@ -55,3 +55,48 @@ def debug(arg):
     from ipydex import IPS
 
     IPS()
+
+@register.tag(name="trim_whitespace_after")
+def trim_whitespace_after(parser, token):
+    """
+    This tag serves to trim newlines and spaces from rendered html – this simplifies the creation of
+    readable templates (with out <!-- ... --> based hacks). Usage
+
+    {% trim_new_lines_after "»" stop_at="«" %}
+    (some text»
+    {% if data.condition %}
+        should be inserted»
+    {% endif %}
+    )
+    {% end_trim_whitespace_after %}
+    """
+    bits = token.split_contents()
+    if len(bits) not in (2, 3):
+        msg = "trimnewlines tag requires one positional argument and accepts one "
+        "optional argument (`stop_at`)."
+        raise template.TemplateSyntaxError(msg)
+
+    start_marker = bits[1].strip("'\"")
+    try:
+        stop_marker = bits[2].split('=', 1)[1].strip("'\"")
+    except IndexError:
+        stop_marker = "«"
+
+    node_list = parser.parse(('end_trim_whitespace_after',))
+    parser.delete_first_token()
+    return TrimSpaceNode(node_list, start_marker, stop_marker)
+
+class TrimSpaceNode(template.Node):
+    def __init__(self, node_list, start_marker, stop_marker):
+        self.node_list = node_list
+        self.start_marker = start_marker
+        self.stop_marker = stop_marker
+
+
+    def render(self, context):
+        output = self.node_list.render(context)
+        parts = output.split(f"{self.start_marker}\n")
+        # Keep the first part as is, lstrip() the rest
+        result = parts[0] + "".join(p.lstrip() for p in parts[1:])
+        results2 = result.replace(self.stop_marker, "")
+        return results2
